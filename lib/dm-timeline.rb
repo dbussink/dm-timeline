7ac40fae27daa98a58172fa0bfe1cf94ab7a4f7c
Dirkjan Bussink <d.bussink@gmail.com>
2008-06-14 21:37:11 +0200

    Add more stuff for default values and relations

--- lib/dm-timeline.rb
+++ lib/dm-timeline.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 gem 'dm-core', '>=0.9.1'
 require 'dm-core'
+require Pathname(__FILE__).dirname.expand_path / 'dm-timeline' / 'adapter_extensions'
 
 module DataMapper
   module Timeline
@@ -15,6 +16,10 @@ module DataMapper
           self.valid_from = options[:at].first
           self.valid_to   = options[:at].last
         end
+
+        self.valid_from = self.class.repository.adapter.class::START_OF_TIME if self.valid_from.nil?
+        self.valid_to   = self.class.repository.adapter.class::END_OF_TIME   if self.valid_to.nil?
+
         save_without_timeline
       end
 
@@ -24,7 +29,7 @@ module DataMapper
       def is_on_timeline
 
         property :valid_from, DateTime, :default => DateTime.now, :nullable => true
-        property :valid_to,   DateTime
+        property :valid_to,   DateTime, :default => repository.adapter.class::END_OF_TIME
 
         include DataMapper::Timeline::InstanceMethods
 
@@ -36,12 +41,20 @@ module DataMapper
             if Hash === query && query.has_key?(:at)
               conditions = query.delete(:at)
             end
-            
+
             unless conditions
-              conditions = [DateTime.now, DateTime.now + 1]
+              conditions = [DateTime.now]
+            else
+              unless conditions.kind_of? Array
+                conditions = [conditions]
+              end
             end
-            
-            all_without_timeline(query.merge(:valid_from.lte => conditions.first, :valid_to.gt => conditions.last))
+
+            if conditions.length < 2 || conditions.first == conditions.last
+              all_without_timeline(query.merge(:valid_from.lte => conditions.first, :valid_to.gt => conditions.first))
+            else
+              all_without_timeline(query.merge(:valid_from.lte => conditions.last, :valid_to.gte => conditions.first))
+            end
           end
 
           def first_with_timeline(options = {})
--- spec/unit/timeline_spec.rb
+++ spec/unit/timeline_spec.rb
@@ -3,6 +3,38 @@ require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
 describe "DataMapper::Timeline" do
 
+  class Stable
+    include DataMapper::Resource
+    include DataMapper::Timeline
+  end
+
+  class Cow
+    include DataMapper::Resource
+    include DataMapper::Timeline
+
+    property :id,        Integer, :serial => true
+    property :name,      String
+    property :breed,     String
+    property :stable_id, Integer
+    belongs_to :stable
+
+    is_on_timeline
+
+    auto_migrate!(:default)
+  end
+
+  class Stable
+    has n, :cows
+
+    property :id,        Integer, :serial => true
+    property :location,  String
+    property :size,      Integer
+
+    is_on_timeline
+
+    auto_migrate!(:default)
+  end
+
   it "is included when DataMapper:Timeline is loaded" do
     Cow.new.should be_kind_of(DataMapper::Timeline)
   end
@@ -13,25 +45,50 @@ describe "DataMapper::Timeline" do
     c.class.properties.map {|p| p.name }.should include(:valid_from)
     c.class.properties.map {|p| p.name }.should include(:valid_to)
 
-    c.valid_from.class.should be(DateTime)
-    c.valid_to.should be_nil
+    c.valid_from.should be_instance_of(DateTime)
+    c.valid_to.should be_instance_of(DateTime)
+    c.valid_to.should eql(c.class.repository.adapter.class::END_OF_TIME)
+  end
+
+  it "sets the maximum and minimum DateTime for the different drivers" do
+    DataMapper::Adapters::AbstractAdapter::START_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::AbstractAdapter::END_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::Sqlite3Adapter::START_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::Sqlite3Adapter::END_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::MysqlAdapter::START_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::MysqlAdapter::END_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::PostgresAdapter::START_OF_TIME.should be_instance_of(DateTime)
+    DataMapper::Adapters::PostgresAdapter::END_OF_TIME.should be_instance_of(DateTime)
   end
 
   it "is able to save the object with a different time period" do
     c = Cow.new
     c.save(:at => [nil, nil])
-    c.valid_from.should be_nil
-    c.valid_to.should be_nil
+    c.valid_from.should be_instance_of(DateTime)
+    c.valid_from.should eql(c.class.repository.adapter.class::START_OF_TIME)
+    c.valid_to.should be_instance_of(DateTime)
+    c.valid_to.should eql(c.class.repository.adapter.class::END_OF_TIME)
   end
 
   it "should be able to find objects on timeline for this instant in time" do
-    c1 = Cow.create(:valid_from => DateTime.new(Date.today.year - 2), :valid_to => DateTime.new(Date.today.year))
-    c2 = Cow.create(:valid_from => DateTime.new(Date.today.year), :valid_to => DateTime.new(Date.today.year + 1))
+    c1 = Cow.new(:name => "Bertha")
+    c1.save(:at => [nil, DateTime.new(Date.today.year - 1)])
+    c2 = Cow.new(:name => "Bertha")
+    c2.save(:at => [DateTime.new(Date.today.year), nil])
 
-    cows = Cow.all
+    cows = Cow.all(:name => "Bertha")
     cows.length.should == 1
     cows.first.should eql(c2)
+  end
+
+  it "should be able to find related objects that are also on timeline" do
+    stable = Stable.create(:location => "MacDonald's Farm")
+    stable.cows << Cow.new(:name => "Cindy 1")
+    stable.cows << Cow.new(:name => "Cindy 2")
+    stable.save
     
+    stable.cows.all(:at => [Date.today, Date.today + 100]).length.should == 2
+    stable.cows.all(:at => [nil, Date.today]).length.should == 0
   end
 
 end
